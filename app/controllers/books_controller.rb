2870e92833534f5290c5deed91eb822e5ee16f6d
minako-y <m.tp2uk@gmail.com>
2021-06-13 14:49:12 +0000

    [Add]book一覧のビュー変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,18 @@
 class BooksController < ApplicationController
   def index
+    @user = User.find(current_user.id)
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
   end
 
   def edit
@@ -15,6 +22,14 @@ class BooksController < ApplicationController
   end
 
   def destroy
+  end
+
+
+
+  private
+
+  def book_params
+    params.permit(:title, :body)
   end
 
 end
